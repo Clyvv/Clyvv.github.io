65a3c59a640c8f352adc95e02fbfa029211ff903
clmawo <clmawo@ttu.ee>
2019-02-26 22:40:06 +0100

    modified php

--- email.php
+++ email.php
@@ -3,13 +3,13 @@ header('Content-Type: application/json');
 if(isset($_POST['submit'])){
     $to = "cmawoko@gmail.com"; 
     $from = $_POST['email']; 
-    $fullName = $_POST['fullName'];
-    $subject = "RSVP for ". $fullName;
+    $full_name = $_POST['full_name'];
+    $subject = "RSVP for ". $full_name;
     $message =  $_POST['message'];
 
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
     mail($to,$subject,$message,$headers);
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    echo "Mail Sent. Thank you " . $full_name . ", we will contact you shortly.";
     }
 ?>
